09cb89cca6d6bd0b1e034ecf1d103bb8b7b1354c
Willem Boshoff <willem.boshoff@dyna-mo.com>
2021-03-25 11:23:45 +0200

    Add Newton's method.

---
+++ Mathematics/NewtonsMethod/NewtonsMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NewtonsMethod
+{
+    /// <summary>
+    /// Newton's method finds the square root of a given value.
+    /// </summary>
+    public static class NewtonsMethod
+    {
+        public static double SquareRoot(double value, double errorMargin = 1e-15)
+        {
+            double squareRoot = value;
+            while(Math.Abs(squareRoot - value / squareRoot) > errorMargin * squareRoot)
+            {
+                squareRoot = (value / squareRoot + squareRoot) / 2.0;
+            }
+            return squareRoot;
+        }
+    }
+}
---
+++ Mathematics/NewtonsMethodTests/SquareRootTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace NewtonsMethodTests
+{
+    public class SquareRootTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public SquareRootTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Theory]
+        [InlineData(2.0)]
+        [InlineData(25.0)]
+        [InlineData(227.0)]
+        [InlineData(236535.0)]
+        [InlineData(2.5)]
+        [InlineData(2245.67)]
+        public void SquareRoot_CompareWithSystemSqrt(double value)
+        {
+            double expected = Math.Round(Math.Sqrt(value), 15);
+            double actual = Math.Round(NewtonsMethod.NewtonsMethod.SquareRoot(value), 15);
+            _output.WriteLine($"Expected = {expected}");
+            _output.WriteLine($"Actual   = {actual}");
+            Assert.Equal(expected, actual);
+        }
+    }
+}
